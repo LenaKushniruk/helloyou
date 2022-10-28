e949ea536aee1f17f424282919cc0b6579f96b0d
LenaKushniruk <114736070+LenaKushniruk@users.noreply.github.com>
2022-10-28 16:21:33 +0200

    Add files via upload

---
+++ helloyou.py
@@ -0,0 +1,75 @@
+print("Het is een prachtige mooie en rustige dag. Je gaat richting de deur en je doet je jas en schoenen aan om naar buiten te gaan. Als je buiten staat zit je de denken welke vervoer ga ik pakken om naar je werk te gaan?")
+print("A. auto")
+print("B. tram")
+print("C. bus")
+x = input("Keuze:") 
+if x == 'A':
+    print("Je hebt uiteindelijk gered om op school te komen. En als je op school bent ga je naar je les toe en zorg je ervoor dat je de lessen volgt en een goede militair wordt.")
+elif x == 'B':
+    print("Wanneer je naar buiten gaat doe je de deur op slot en je loopt richting de bus of tramhalte. En tijdens het lopen zie een iemand heel verdacht kijken naar een oude vrouw en een man. De man zie je heel voorzichtig en langzaam naar het oude stel toe gaan en ziet iets uit hun zak pakken. Je besluit om….")  
+    print("A. er wat van te zeggen en gaat erachter aan")
+    print("B. of je besluit om het tegen iemand te zeggen en vraagt of ze de politie belt")
+if x == 'C':
+    print("Je ben in de bus gestapt na een paar haltes zie je steeds dat er steeds meer mensen in de bus stappen. Dus je beluit om naar de buschauffeur te gaan en te vragen hoe lang het duurt, maar je kijkt even snel naar het nieuws online en je leest dat er super veel vluchtelingen bij elke bushalte staan om met de bus mee te gaan. Besluit je om….")
+    print("A. uit de bus te stappen en verder te gaan lopen.")
+    print("B. in de bus te blijven zitten.")
+    x = input("Keuze:")
+    if x == 'A':
+        print("De bus stopt en je stapt uit de bus om verder te gaan lopen. Onderweg naar je school app je je klasgenoten om te vertellen dat je het nieuws hebt gelezen en zegt dat het ongelofelijk druk is in het openbaar vervoer. Je ziet 2 wegen die ergens naar toe leiden te ene gaat naar je school en de andere gaat naar de toekomst. Welke ga je kiezen?")
+        print("A. De weg naar school ")
+        print("B. De weg naar de toekomst ")
+        x = input("Keuze:")
+        if x == 'A':
+            print("Je ziet dat je veel naar school hebt gelopen en je kijkt op je telefoon hoe laat het is en binnen mum van tijd ben. Vervolg")
+            x = input("E")
+            print("Je bent op school. En als je op school bent ga je naar je les toe en zorg je ervoor dat je de lessen volgt en een goede ervaren militair wordt.")
+        if x == 'B':
+            print("Je hebt gekozen om de toekomst weg te kiezen je volgt deze weg en na een tijd zie je dat de wereld super erg is veranderd. Je ziet dat er super veel vluchtelingen robots het werk van de mensen hebben overgenomen. En je komt een arme man tegen besluit je tegen hem te praten?")
+            print("A. Ja")
+            print("B. Nee")
+            x = input("Keuze:")
+            if x == 'A':
+                print("Je spreekt de arme man aan en vraagt wat er met hem is gebeurd. En hij zegt het volgende: ` Ik ben hier op straat gekomen doordat deze irritante robots mijn baan hebben overgenomen en veel van mijn collega’s die ik ken zijn ook op straat beland.’ Na het verhaal gehoord te hebben ga je een keuze maken.")
+                print("A. Het verleden")
+                print("B. Toekomst")
+                x = input("Keuze:")
+                if x == 'A':
+                    print("Je gaat door een tijdmachine en komt in het verleden van de mensen die op straat zijn en verteld hun wat er gaat gebeuren met hen en hoe het eruit gaat zien na een aantal jaar. Ga verder ")
+                    x = input("E")
+                    print("Na het uitstappen van de tijdmachine gaat de tijd machine opeens kapot en kan je niet meer terug naar je huidige tijd.")
+                if x == 'B':
+                    print("Je loopt door, maar in de verte zie je op het plein dat er super veel mensen die ontslagen zijn, omdat hun werden overgenomen besloten zij te protesteren. Vervolg")
+                x = input("E")
+                print("Je komt dichterbij bij de mensen en begint te zoeken naar de man met wie je hebt gesproken en tikt op zijn schouder. Hij draait zich om en zegt: ’Wie ben je? Ken ik jou van ergens?’. En ik zeg daarop:")
+                print("A. Ja")
+                print("B. Nee")
+                x = input("Keuze:")
+                if x == 'A':
+                    print("Door de informatie te geven over de man gelooft de man en besluit te luisteren en na een tijdje zie je de robot vluchtelingen het bijzondere tijdmachine kapot maken en je kan dus niet meer terug en blijf je hier. ")
+                elif x == 'B':
+                    print("Je hebt besloten om voor deze keuze te gaan en blijft in de huidige tijd. En kan niet meer terug en blijft in deze eeuw. ")
+    elif x == 'B':
+        print("Je blijft in de bus zitten en telkens na elke halte komen er steeds meer mensen in de bus. Dus als de bus bij elke halte is stopt hij heel even. De bus blijft even staan a = ga je de buschauffeur roepen of b=schreeuwen ?")
+        print("A. a")
+        print("B. b") 
+        x == input("Keuze")  
+        if x == 'A' + 'B':
+            print("De buschauffeur heb je gezien en gehoord en doet de deur voor je open zodat je eruit kan gaan. Je probeert je eruit te wurmen en dat is gelukt als je uit de bus bent ga je bellen naar familie en vrienden en vragen of zij jou kunnen brengen. Maar hun telefoon gaat niet over. Dus doodlopende einde")          
+x = input("Keuze:")    
+if x == 'A':
+    print("Door de politie te waarschuwen grijpt de politie je bij de hand en trekt jou mee achter de auto om zo veilig voor jouw te maken. Als dit voorbij is vraag je de politie of hun jou naar school kunnen brengen, omdat je veilig bij hun in de auto voelt. En de politie zegt: ‘Natuurlijk doen wij dat!’. Ga verder.. ")
+    x = input('E') 
+    print("Je bent op school.En als je op school bent ga je naar je les toe en zorg je ervoor dat je de lessen volgt en een goede ervaren militair wordt")  
+elif x == 'B':
+    print("Je ziet iemand bij de bushalte staan en je vertelt dat je een zakkenroller heb gezien en vraagt of hij/zij de politie wilt bellen. Na een tijdje is de politie er al en word de man aangehouden. Vervolg....")  
+    x = input('E') 
+    print("Nadat de man wordt opgepakt zie je iemand rennen met een geweer op je af. Wat ga je doen?")
+    print("A. Je waarschuwt de politie")
+    print("B. Je doet helemaal niks door de angst")
+    x = input("Keuze:")
+    if x == 'A':    
+        print("Door de politie te waarschuwen grijpt de politie je bij de hand en trekt jou mee achter de auto om zo veilig voor jouw te maken. Als dit voorbij is vraag je de politie of hun jou naar school kunnen brengen, omdat je veilig bij hun in de auto voelt. En de politie zegt: ‘Natuurlijk doen wij dat!’. Ga verder")
+        x == input('E')
+        print("Je bent op school. En als je op school bent ga je naar je les toe en zorg je ervoor dat je de lessen volgt en een goede ervaren militair wordt.")
+    if x == 'B':
+        print("Je hebt gekozen om de gevaarlijke keuze te maken. En je overlijdt.")
